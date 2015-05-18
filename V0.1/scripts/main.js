1be077e9c5488b6f037a0376d41d862d1dff141f
Alex Berneau <haldir.alex@gmail.com>
2015-05-19 00:25:24 +0200

    V0.2
    -ajout du logo (provisoire)
    -ajout du goban (provisoire)

--- V0.1/scripts/main.js
+++ V0.1/scripts/main.js
@@ -1,6 +1,16 @@
-// Simple jQuery event handler
+/* Simple jQuery event handler
 $(document).ready(function(){
 
 
 
-});
+}); */
+
+function pion_bleue()
+{
+	document.getElementById("bleue").innerHTML="<img src='images/pion_bleue.png' alt='bleue'>"
+}
+
+function pion_rose()
+{
+	document.getElementById("rose").innerHTML="<img src='images/pion_rose.png' alt='rose'>"
+}
