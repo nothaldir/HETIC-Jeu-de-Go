539feac0a3ed70eecfe0ca08555ccbec77f95443
Gala Pillot <gala.pill@gmail.com>
2015-05-26 16:16:58 +0200

    test

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ on code actuellement en itératif (suite d instructions)
 
 //Creating the goban with JS ==>
 	
-//If you wish to change the bogan size, change 9 by 13 or 19
+//If you wish to change the goban size, change 9 by 13 or 19
 var boardSize=9-1;
 //Calculating the number of blocks (and divs) for a 9x9 goban
 boardSize=boardSize*boardSize;
@@ -28,13 +28,24 @@ var cells = document.getElementById('goban_back');
 var row = 0;
 var column = 0;
 
+var tab=new Array; // Tableau multidimensionnel pour stocker valeurs de chaque case (P1 = 1, P2 = 2, empty = 0)
+
+for(var x=0; x<9; x++)
+   tab[x] = new Array();
+
+for(var x=0; x<9; x++)
+   // ... et dans chaque ligne, on parcourt les cellules
+   for(var y=0; y<9; y++)
+      tab[x][y] = 0;
+
+
 //Creating the goban blocks
 for (var i = boardSize; i > 0; i--) 
 {
     cells.innerHTML = cells.innerHTML + "<div class='back'> </div>";        
 };
 
-//Calculating the number of intersections to place the pons 
+//Calculating the number of intersections to place the powns 
 boardSize=9*9;
 var cells = document.getElementById('goban_front');
 
@@ -42,7 +53,7 @@ var cells = document.getElementById('goban_front');
 //Creating the intersections
 for (var i = boardSize; i > 0; i--) 
 {
-    cells.innerHTML = cells.innerHTML + "<div class='front' id='"+row+"_"+column+"' onClick=\"test1('"+row+"_"+column+"')\"> </div>";
+    cells.innerHTML = cells.innerHTML + "<div class='front' id='"+row+"_"+column+"' onClick=\"basic('"+row+"_"+column+"')\"> </div>";
   	column++;
   	if (column==9) 
   	{
@@ -51,28 +62,91 @@ for (var i = boardSize; i > 0; i--)
   	};         
 };
 
-//Functions to identify the intersections, add pons etc.
+//Functions to identify the intersections, add powns etc.
 
 var player = 1;
 
-function test1(id) {
+var round = 0;
+
+
+// on cree le tableau bleu, contenant les lignes
+var tab = new Array();
+
+// on cree les lignes (tableau vert) les unes après les autres
+for(var i=0; i<9; i++)
+   tab[i] = new Array();	
+
+// on parcourt les lignes...
+for(var i=0; i<9; i++)
+   // ... et dans chaque ligne, on parcourt les cellules
+   for(var j=0; j<9; j++)
+      tab[i][j] = 0;
+
+console.log(tab);
+
+function basic(id) {
     var x_y = id.indexOf("_");
     var x = parseInt(id.substring(0, x_y));
     console.log("x="+x);
     var y = parseInt(id.substring(x_y + 1));
     console.log("y="+y);
-}
-
-function update()
-{
-	if (player==1)
+	
+	if (player==1 && tab[x][y]==0)
 	{
-		var element = document.getElementById(i + "_" + j);
-        element.createElement("div").setAttribute("class", "player1");	
+			var element = document.getElementById(x+"_"+y);
+			element.innerHTML="<div class='player1'> </div>";
+			player=2;
+			round++;	
+			console.log("C'est le tour n°"+round);
+			tab[x][y] = player;		
 	}
-	else if(player==2)
+	else if(player==2 && tab[x][y]==0)
 	{
-		var element = document.getElementById(i + "_" + j);
-        element.createElement("div").setAttribute("class", "player2");	
+			var element = document.getElementById(x+"_"+y);
+			element.innerHTML="<div class='player2'> </div>";        
+			player=1;
+			console.log("C'est le tour n°"+round);
+			tab[x][y] = player;			
 	};
 }
+
+// Function miam
+
+function miam() {
+    for (var x = 0; x < rows; x++) {
+        for (var y = 0; y < rows; y++) {
+			if (tab[x][y]==1 && tab[x-1][y]==2 && tab[x][y-1]==2 && tab[x][y+1]==2 && tab[x+1][y]==2) {
+				tab[x][y]=0;
+			}
+			else if (tab[x][y]==2 && tab[x-1][y]==1 && tab[x][y-1]==1 && tab[x][y+1]==1 && tab[x+1][y]==1) {
+				tab[x][y]=0;
+			}
+		}	
+	}
+};
+
+// Function suicide (interdit au joueur de poser son pion sur une case vide si celle ci est entourée par 4 pions de même famille)
+
+function suicide() {
+	if (tab[x][y]==0 && tab[x-1][y]==1 && tab[x][y-1]==1 && tab[x][y+1]==1 && tab[x+1][y]==1) {
+		if (tab[x-1][y-1]==2 && tab[x-2][y]==2 && tab[x-2][y+1]==2) { // possibilité 1
+			// tu ne peux pas peux jouer si au tour d'avant, P1 a bouffé ton pion
+			// sinon tu peux jouer
+		}
+		else if (tab[x][y-2]==2 && tab[x-1][y-1]==2 && tab[x+1][y-1]==2) { // possibilité 2
+			// idem
+		}
+		else if (tab[x][y+2]==2 && tab[x-1][y+1]==2 && tab[x+1][y+1]==2) { // possibilité 3
+			// idem
+		}
+		else if (tab[x+1][y-1]==2 && tab[x+2][y]==2 && tab[x+1][y+1]==2) { // possibilité 4
+			// idem
+		}
+		alert("t'es con ou quoi?");
+	}
+	else if (tab[x][y]==0 && tab[x-1][y]==2 && tab[x][y-1]==2 && tab[x][y+1]==2 && tab[x+1][y]==2) {
+		alert("t'es con ou quoi?");
+	}
+}
+
+
