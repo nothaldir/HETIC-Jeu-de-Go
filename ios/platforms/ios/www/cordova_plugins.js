87d888a302d43496f59c487663db096bf445bc3c
Nans THOMAS <nans.thomas@hetic.net>
2015-06-20 05:22:59 +0200

    🚬 Pause clope release
    - Ajout de plein de truc.

--- ios/platforms/ios/www/cordova_plugins.js
+++ ios/platforms/ios/www/cordova_plugins.js
@@ -13,13 +13,21 @@ module.exports = [
         "clobbers": [
             "gamecenter"
         ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.statusbar": "0.1.10",
-    "cordova-plugin-game-center": "0.4.1"
+    "cordova-plugin-game-center": "0.4.1",
+    "nl.x-services.plugins.socialsharing": "4.3.18"
 }
 // BOTTOM OF METADATA
 });
