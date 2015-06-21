4617c2b91aef475d680344083eaac717e076459f
Alex Berneau <haldir.alex@gmail.com>
2015-06-21 15:29:42 +0200

    Le commit du simplet.

--- ios/www/scripts/main.js
+++ ios/www/scripts/main.js
@@ -1,24 +1,3 @@
-/*
-construction du jeu
-1. développer le goban
-	tableau avec classe pour chaque TD (déconseillé fortement)
-	div à coté des autres avec un float left pour retour à la ligne après 19 classe
-2. repérer et récup les cases
-	préférable dans tableau à deux dimensions (X et Y)
-3. générer plateau en JS pour modif easy plus tard
-4. créer un fichier js avec toutes les variables
-5. à qui le tour et pion adéquat (variable pour joueur en cours)
-6. regarder les disponibilités autour et en choisir une au hasard
-7. repérer si partie finie, groupe mangé etc...
-
-Récursivité : fonction qui s apelle elle même
-on code actuellement en itératif (suite d instructions)
-
-A RETENIR : VARIABLES LOCALES ET GLOBALES (avec ou sans var) !!!
-
-*/
-
-
 //Creating the goban with JS ==>
 
 //If you wish to change the goban size, change 9 by 13 or 19
@@ -51,7 +30,6 @@ for (var i = boardSize; i > 0; i--)
 
 //Functions to identify the intersections, add powns etc.
 
-var player = 1;
 
 var round = 0;
 
@@ -60,7 +38,11 @@ var pointJ2 = 0;
 
 var suicide = false;
 
-var waitingPlayer = 2;
+var player = 1;
+var nextPlayer = 2;
+
+var previousJ1 = null;
+var PreviousJ2 = null;
 
 
 // on cree le tableau bleu, contenant les lignes
@@ -79,25 +61,25 @@ console.log(tab);
 
 
 //Tableau groupes
-var tabM = new Array();
+var groups = new Array();
 for(var x=0; x<9; x++)
-   tabM[x] = new Array();
+   groups[x] = new Array();
 for(var x=0; x<9; x++)
    for(var y=0; y<9; y++)
-      tabM[x][y] = 0;
+      groups[x][y] = 0;
 console.log("tableau des groupes")
-console.log(tabM);
+console.log(groups);
 
 
 // Tableau qui stock les tours
-var rounds = new Array();
+var takes = new Array();
 for(var x=0; x<9; x++)
-   rounds[x] = new Array();
+   takes[x] = new Array();
 for(var x=0; x<9; x++)
    for(var y=0; y<9; y++)
-      rounds[x][y] = null;
+      takes[x][y] = null;
 console.log("Tableau tours");
-console.log(rounds);
+console.log(takes);
 
 
 function basic(id) {
@@ -106,91 +88,62 @@ function basic(id) {
     console.log("x="+x);
     y = parseInt(id.substring(x_y + 1));
     console.log("y="+y);
+    console.log("player : " + player);
+    console.log("--------------");
+    console.log(round);
 
-    // suicideCheck();
+    detectGroup();
+    suicideCheck();
 
-	if (player==1 && tab[x][y]==0 && suicide==false && rounds[x][y]!=round-2)
+	if (tab[x][y]!=player && tab[x][y]==0 && suicide==false && takes[x][y]!=round)
 	{
-		// var element = document.getElementById(x+"_"+y);
-		// element.innerHTML="<div class='player1'> </div>";
 		round++;
 		tab[x][y] = player;
-		rounds[x][y] = round;
-		// player=2;
-		console.log("coucoucocuocucoucoucuocu");
-	}
-	else if(player==2 && tab[x][y]==0 && suicide==false)
-	{
-		// var element = document.getElementById(x+"_"+y);
-		// element.innerHTML="<div class='player2'> </div>";
-		round++;
-		tab[x][y] = player;
-		rounds[x][y] = round;
-		// player=1;
-	}
-
-	//miam();
-	detectGroup();
-	capture();	
-	maj();
-	playerTurn(); 	
-}
-
-/* Function miam
-
-function miam() {
-    for (var x = 0; x < row; x++) {
-        for (var y = 0; y < row; y++) {
-			if (tab[x][y]==1 && tab[x-1][y]==2 && tab[x][y-1]==2 && tab[x][y+1]==2 && tab[x+1][y]==2) {
-				tab[x][y]=0;
-				console.log("le pion "+x+"_"+y+" fut miam");
-				document.getElementById(x+"_"+y).innerHTML=" ";
-				pointJ1++;
-				console.log("J1, nombre de points = "+pointJ1);
-			}
-			else if (tab[x][y]==2 && tab[x-1][y]==1 && tab[x][y-1]==1 && tab[x][y+1]==1 && tab[x+1][y]==1) {
-				tab[x][y]=0;
-				console.log("le pion "+x+"_"+y+" fut miam");
-				document.getElementById(x+"_"+y).innerHTML=" ";
-				pointJ2++;
-				console.log("J2, nombre de points = "+pointJ2);
-			}
-		}
+		takes[x][y] = round;
+        detectGroup();
+        capture();  
+        maj();
+        playerTurn();  
+        if (player==1)
+        {
+            previousJ1=x+""+y;
+            console.log(previousJ1);
+        } 
 	}
 }
 
-*/
 
 function capture()
 {
-    if ( (y-1)>=0 && tab[x][y-1]==waitingPlayer)
+    if ( (y-1)>=0 && tab[x][y-1]==nextPlayer)
     {
         libertiesGroup(x, y-1);
     }
-    if ((x+1)<row && tab[x+1][y]==waitingPlayer)
+    if ((x+1)<row && tab[x+1][y]==nextPlayer)
     {
         libertiesGroup(x+1, y);
     }
-    if ((y+1)<row && tab[x][y+1]==waitingPlayer)
+    if ((y+1)<row && tab[x][y+1]==nextPlayer)
     {
         libertiesGroup(x, y+1);
     }
-    if ((x-1)>=0 && tab[x-1][y]==waitingPlayer)
+    if ((x-1)>=0 && tab[x-1][y]==nextPlayer)
     {
         libertiesGroup(x-1, y);
     }
 }
 
 
+
 function libertiesGroup (x,y)
 {
     detectGroup();
-    var groupeNum = tabM[x][y];
+    var groupeNum = groups[x][y];
     for (var i=0; i<row; i++)
     {
         for (var j=0; j<row; j++)
         {
-            if (tabM[i][j]==groupeNum && groupeNum!=0)
+            if (groups[i][j]==groupeNum && groupeNum!=0)
             {
                 if ( ((j-1)>=0 && tab[i][j-1]==0)  || ((i+1)<row && tab[i+1][j]==0) || ((j+1)<row && tab[i][j+1]==0) || ((i-1)>=0 && tab[i-1][j]==0) )
                 {
@@ -207,9 +160,11 @@ function libertiesGroup (x,y)
     {
         for (var j=0; j<row; j++)
         {
-            if (tabM[i][j]==groupeNum)
+            if (groups[i][j]==groupeNum)
             {
                 tab[i][j] = 0;
+                takes[i][j] = round;
+
                 //prisonniersJoueur ++;
                 // Donc on remet à 0 les pions capturés du groupe et on increment la variable de
             }
@@ -219,9 +174,11 @@ function libertiesGroup (x,y)
 
 
 
+
 // Function suicide (interdit au joueur de poser son pion sur une case vide si celle ci est entourée par 4 pions de même famille)
 function suicideCheck() {
-	if (tab[x][y]==0 && tab[x-1][y]==1 && tab[x][y-1]==1 && tab[x][y+1]==1 && tab[x+1][y]==1) {
+
+	if ( (tab[x][y]==0) && ((x-1)<=0 || tab[x-1][y]==1) && ((y-1)>=0 || tab[x][y-1]==1) && ((y+1)>=row || tab[x][y+1]==1) && ((x+1)>=row || tab[x+1][y]==1)) {
 		if (tab[x-1][y-1]==2 && tab[x-2][y]==2 && tab[x-1][y+1]==2) {
 			// possibilité 1
 			// tu ne peux pas peux jouer si au tour d'avant, P1 a bouffé ton pion
@@ -250,7 +207,7 @@ function suicideCheck() {
 			console.log("suicide = "+suicide);
 		}
 	}
-	else if (tab[x][y]==0 && tab[x-1][y]==2 && tab[x][y-1]==2 && tab[x][y+1]==2 && tab[x+1][y]==2) {
+	else if ( (tab[x][y]==0) && ((x-1)<=0 || tab[x-1][y]==2) && ((y-1)>=0 || tab[x][y-1]==2) && ((y+1)>=row || tab[x][y+1]==2) && ((x+1)>=row || tab[x+1][y]==2)) {
 		if (tab[x-1][y-1]==1 && tab[x-2][y]==1 && tab[x-1][y+1]==1) {
 			// possibilité 1
 			// tu ne peux pas peux jouer si au tour d'avant, P1 a bouffé ton pion
@@ -286,16 +243,17 @@ function suicideCheck() {
 	}
 }
 
+
 function detectGroup() {
     var group_id = 1;
     for (var x=0; x < row; x++) {
         for (var y=0; y < row; y++) {
             if (tab[x][y] == 0) {
                 // si ya rien bah ya rien
-                tabM[x][y] = 0;
+                groups[x][y] = 0;
             }
             else {
-                tabM[x][y] = group_id;
+                groups[x][y] = group_id;
                 group_id++;
             }
         }
@@ -304,44 +262,44 @@ function detectGroup() {
         for (y = 0; y < row; y++) {
             // Left
             if ((y - 1) >= 0 && tab[x][y] == tab[x][y - 1]) {
-                var former_group = tabM[x][y - 1];
+                var former_group = groups[x][y - 1];
                 for (var k = 0; k < row; k++) {
                     for (var l = 0; l < row; l++) {
-                        if (tabM[k][l] == former_group) {
-                            tabM[k][l] = tabM[x][y];
+                        if (groups[k][l] == former_group) {
+                            groups[k][l] = groups[x][y];
                         }
                     }
                 }
             }
             // Down
             if ((x + 1) > row && tab[x][y] == tab[x + 1][y]) {
-                var former_group = tabM[x + 1][y];
+                var former_group = groups[x + 1][y];
                 for (k = 0; k < row; k++) {
                     for (l = 0; l < row; l++) {
-                        if (tabM[k][l] == former_group) {
-                            tabM[k][l] = tabM[x][y];
+                        if (groups[k][l] == former_group) {
+                            groups[k][l] = groups[x][y];
                         }
                     }
                 }
             }
             // Right
             if ((y + 1) < row && tab[x][y] == tab[x][y + 1]) {
-                var former_group = tabM[x][y + 1];
+                var former_group = groups[x][y + 1];
                 for (var k = 0; k < row; k++) {
                     for (var l = 0; l < row; l++) {
-                        if (tabM[k][l] == former_group) {
-                            tabM[k][l] = tabM[x][y];
+                        if (groups[k][l] == former_group) {
+                            groups[k][l] = groups[x][y];
                         }
                     }
                 }
             }
             // Up
             if ((x - 1) >= 0 && tab[x][y] == tab[x - 1][y]) {
-                var former_group = tabM[x - 1][y];
+                var former_group = groups[x - 1][y];
                 for (var k = 0; k < row; k++) {
                     for (var l = 0; l < row; l++) {
-                        if (tabM[k][l] == former_group) {
-                            tabM[k][l] = tabM[x][y];
+                        if (groups[k][l] == former_group) {
+                            groups[k][l] = groups[x][y];
                         }
                     }
                 }
@@ -350,19 +308,23 @@ function detectGroup() {
     }
 }
 
+
+
 function playerTurn ()
 {
     if (player == 1)
     {
-        waitingPlayer = 1;
+        nextPlayer = 1;
         player = 2;
     }  
-    else
+    else if (player == 2)
     {
-        waitingPlayer = 2;
+        nextPlayer = 2;
         player = 1;   
     }
 }
+
+
 
 function maj() {
     for (var i = 0; i < row; i++) {
@@ -371,19 +333,16 @@ function maj() {
             {
                 var element = document.getElementById(i+"_"+j);
 				element.innerHTML=" ";
-				console.log("text0");
             }
             else if (tab[i][j] == 1) 
             {
                 var element = document.getElementById(i+"_"+j);
 				element.innerHTML="<div class='player1'> </div>";
-				console.log("text1");
             } 
             else if (tab[i][j] == 2) 
             {
                 var element = document.getElementById(i+"_"+j);
 				element.innerHTML="<div class='player2'> </div>";
-				console.log("text2");
             }
         }
     }
